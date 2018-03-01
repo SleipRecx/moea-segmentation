1db8975b353ee7956996b744a22047df511f7f92
corneliusgdahling <corneliusgdahling@gmail.com>
2018-03-01 12:06:52 +0100

    Added function for pixel to calculate color distance

--- main/utils.go
+++ main/utils.go
@@ -8,6 +8,11 @@ func colorDistance(p1 Pixel, p2 Pixel) float64 {
 	r := math.Pow(float64(p1.R - p2.R), 2)
 	g := math.Pow(float64(p1.G - p2.G), 2)
 	b := math.Pow(float64(p1.B - p2.B), 2)
+	a := math.Pow(float64(p1.A - p2.A), 2)
 
-	return math.Sqrt(r + g + b)
+	return math.Sqrt(r + g + b + a)
+}
+
+func (p1 Pixel) distance (p2 Pixel) float64 {
+	return colorDistance(p1, p2)
 }
