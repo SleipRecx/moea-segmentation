18e7f84b7e3e675cb5b6c16cf1fb7c6f88f478f6
Markus Andresen <markus.andresen94@gmail.com>
2018-03-05 02:57:23 +0100

    Variable name refactor for readability

--- src/main/main.go
+++ src/main/main.go
@@ -15,12 +15,11 @@ func main() {
 	path := "./test_images/"
 	folderNumber := "1"
 	myImage := image.ReadImageFromFile(path, folderNumber)
-	graph := image.ImageToGraph(myImage)
-	fmt.Println(len(graph.Vertices))
-	mst := graph.MinimalSpanningTree()
-	segments := mst.RandomDisjointPartition(10)
+	imageGraph := image.ImageToGraph(myImage)
+	minimalSpanningTree := imageGraph.MinimalSpanningTree()
+	segments := minimalSpanningTree.RandomDisjointPartition(10)
 	segmentedImage := image.ReconstructImage(segments, myImage)
 	image.SaveImageToFile(segmentedImage)
-	end := time.Now()
-	fmt.Println(end.Sub(start))
+
+	fmt.Println("Total runtime:", time.Now().Sub(start))
 }
