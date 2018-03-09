6639c767a69f3a25be0c3c643868f41da07e6206
Markus Andresen <markus.andresen94@gmail.com>
2018-03-09 11:09:21 +0100

    main ready for corry

--- src/main/main.go
+++ src/main/main.go
@@ -16,15 +16,18 @@ func main() {
 
 	myImage := image.ReadImageFromFile(path, folderNumber)
 	imageGraph := myImage.ConvertToGraph()
-	segments := imageGraph.GraphSegmentation(1000)
+	segments := imageGraph.GraphSegmentation(100000)
 	c := chromosome.NewChromosome(segments, myImage)
-
-
-	fmt.Println(c.CalcEdgeValue())
-	fmt.Println("Total runtime:", time.Now().Sub(start))
 
 	segmentedImage := image.ReconstructImage(c.Segments, myImage)
 	image.SaveImageToFile(segmentedImage)
+	fmt.Println("Total runtime:", time.Now().Sub(start))
+	fmt.Println(c.CalcEdgeValue())
+	fmt.Println("Total runtime:", time.Now().Sub(start))
+
+
+
+
 
 
 
