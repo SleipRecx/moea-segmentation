31fad5564b6b3ed3f0127ba8c89795c762ad3976
corneliusgdahling <corneliusgdahling@gmail.com>
2018-03-11 18:42:41 +0100

    Method to run tester

--- src/main/main.go
+++ src/main/main.go
@@ -2,21 +2,35 @@ package main
 
 import (
 	"../img"
-	"../tester"
+	//"../tester"
 	"fmt"
 	"time"
 
+	"io/ioutil"
+	"strings"
+	"../tester"
 )
 
 func main() {
 	start := time.Now()
 	img.Path = "./test_images/"
-	img.FolderNumber = "1"
+	img.FolderNumber = "dessert"
 	img.MyImage = img.ReadImageFromFile(img.Path, img.FolderNumber)
 	img.ImageWidth, img.ImageHeight = len(img.MyImage.Pixels), len(img.MyImage.Pixels[0])
 	img.MyImageGraph = img.MyImage.ConvertToGraph()
 
-	tester.CalculatePRI(ScriptPath, TestPath + img.FolderNumber, OutPath)
+	file, _ := ioutil.ReadFile("src/main/path.txt")
+	split := strings.Split(string(file), "\n")
+	scriptPath := split[0]
+	testPath := split[1] + img.FolderNumber
+	outPath := split[2]
+
+	fmt.Println(scriptPath)
+	fmt.Println(testPath)
+	fmt.Println(outPath)
+
+
+	tester.CalculatePRI(scriptPath, testPath, outPath)
 
 	fmt.Println("Total runtime:", time.Now().Sub(start))
 
