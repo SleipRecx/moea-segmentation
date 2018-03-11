ef6d965c311d77dfebf6f6fadfd6944738669295
corneliusgdahling <corneliusgdahling@gmail.com>
2018-03-11 17:56:29 +0100

    Changed path of edge detection and added to gitignore to create file with const for paths

--- src/main/main.go
+++ src/main/main.go
@@ -17,15 +17,10 @@ func main() {
 	img.ImageWidth, img.ImageHeight = len(img.MyImage.Pixels), len(img.MyImage.Pixels[0])
 	img.MyImageGraph = img.MyImage.ConvertToGraph()
 
-	scriptPath := "/Users/corneliusgriegdahling/Documents/MOEA/src/tester/run.py"
-	testPath := "/Users/corneliusgriegdahling/Documents/MOEA/test_images/" + img.FolderNumber
-	outPath := "/Users/corneliusgriegdahling/Documents/MOEA/output/edge/"
-
-
 
 	phenotype := ga.NewPhenotype()
 	img.SaveEdgeDetectionImage(phenotype.Segments, img.MyImage, phenotype.SegmentMap, img.FolderNumber)
-	tester.CalculatePRI(scriptPath, testPath, outPath)
+	tester.CalculatePRI(ScriptPath, TestPath + img.FolderNumber, OutPath)
 	rImage := img.ReconstructImage(phenotype.Segments)
 	img.SaveImageToFile(rImage, "fuuck")
 	fmt.Println("Total runtime:", time.Now().Sub(start))
